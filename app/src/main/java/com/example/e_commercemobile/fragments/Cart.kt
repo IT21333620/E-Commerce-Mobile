7dfa8c2cd91af42c295b6bba972f23f223935149
Kavija Sapukotana <it21333620@my.sliit.lk>
2024-09-29 23:55:31 +0530

    recycler view ui creation

--- app/src/main/java/com/example/e_commercemobile/fragments/Cart.kt
+++ app/src/main/java/com/example/e_commercemobile/fragments/Cart.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.example.e_commercemobile.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class cart : Fragment() {
 
+    private lateinit var checkOut: FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +23,20 @@ class cart : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+        val view = inflater.inflate(R.layout.fragment_cart, container, false)
+
+        checkOut = view.findViewById(R.id.checkOutFloatingBtn)
+
+        checkOut.setOnClickListener{
+            val fragment = CheckOut()
+            val fragmentManager = activity?.supportFragmentManager
+            val fragmentTransaction = fragmentManager?.beginTransaction()
+            fragmentTransaction?.replace(R.id.fragmentContainerView, fragment)
+            fragmentTransaction?.addToBackStack(null)
+            fragmentTransaction?.commit()
+        }
+
+        return view
     }
 
 }
